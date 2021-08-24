3284a02c0c12e2d052fed536a633853929e795bf
Victor <mfzigzag@gmail.com>
2021-08-24 22:01:25 +0300

    create: added 'Swiper' slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const mySwiper = new Swiper('.swiper-container', {
+   // Optional parameters
+   loop: true,
+
+
+   // Navigation arrows
+   navigation: {
+      nextEl: '.slider-button--next',
+      prevEl: '.slider-button--prev',
+   },
+});
