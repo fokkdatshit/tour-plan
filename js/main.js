95ac60a026da77748ecd3a8b132f6b67b7a8f785
Victor <mfzigzag@gmail.com>
2021-08-27 06:48:20 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -21,3 +21,9 @@ const reviewsSlider = new Swiper('.reviews-slider', {
       prevEl: '.reviews-slider__button--prev',
    },
 });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+   console.log("sada");
+   document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+});
